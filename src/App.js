12eb963ea5eba96a2244a594abd8b88c555b97eb
jacquesvzyl <jacquesf.vanzyl@gmail.com>
2022-06-23 11:38:04 +0700

    Added google analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import ContextProvider from "./store/ContextProvider";
-
+import ReactGA from "react-ga";
 import Desktop from "./components/UI/desktop/Desktop.component";
+const TRACKING_ID = "G-MF58SPC4P8"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
