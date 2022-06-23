c653ae5943d8a75673ffa6e6262b3e0e8501b129
jacquesvzyl <jacquesf.vanzyl@gmail.com>
2022-06-23 12:37:14 +0700

    added google analystics

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactGA from "react-ga";
 import Desktop from "./components/UI/desktop/Desktop.component";
 const TRACKING_ID = "G-MF58SPC4P8"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
